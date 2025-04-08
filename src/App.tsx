f142fe4557cf2764b08a2fdea21627a865be6ddb
Dairan99 <vadim.parshenkov@mail.ru>
2025-04-08 15:28:25 +0300

    summary 5.5

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import AboutAccount from './pages/AboutAccount';
 function App() {
 	return (
 		<>
-			<HashRouter basename="/ReactFinal" future={{
+			<HashRouter future={{
                     v7_startTransition: true,
                     v7_relativeSplatPath: true,
                 }}>
